4bec09281901f72e8b0bc10ea4eae176d53a2c29
vaughanstech <vaughanstech@gmail.com>
2022-08-05 22:56:36 -0400

    added the power_loss function

--- ohms-law.cpp
+++ ohms-law.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <math.h>
+#include <stdio.h>
 
 void ohms_law()
 {
@@ -35,14 +37,54 @@ void ohms_law()
     }
     else
     {
-        std::cout << "\nThe selected an invalid mode, try again...\n";
+        std::cout << "\nYou selected an invalid mode, try again...\n";
         ohms_law();
+    }
+}
+
+void power_loss()
+{
+    int mode;
+    float i, r, v;
+    std::cout << "1. Current & Voltage\n2. Current & Resistance\n3. Voltage & Resistance\nWhat values are known?: ";
+    std::cin >> mode;
+    if (mode == 1)
+    {
+        std::cout << "Calculating power loss...\n";
+        std::cout << "Enter the Current (I): ";
+        std::cin >> i;
+        std::cout << "Enter the Voltage (V): ";
+        std::cin >> v;
+        std::cout << "Power: " << i * v << "\n";
+    }
+    else if (mode == 2)
+    {
+        std::cout << "Calculating power loss...\n";
+        std::cout << "Enter the Current (I): ";
+        std::cin >> i;
+        std::cout << "Enter the Resistance (R): ";
+        std::cin >> r;
+        std::cout << "Power: " << pow(i, 2) * r << "\n";
+    }
+    else if (mode == 3)
+    {
+        std::cout << "Calculating power loss...\n";
+        std::cout << "Enter the Voltage (V): ";
+        std::cin >> v;
+        std::cout << "Enter the Resistance (R): ";
+        std::cin >> r;
+        std::cout << "Power: " << pow(v, 2) * r << "\n";
+    }
+    else
+    {
+        std::cout << "\nYou selected an invalid mode, try again...";
+        power_loss();
     }
 }
 
 int main()
 {
-    ohms_law();
+    power_loss();
 
     return 0;
 }
